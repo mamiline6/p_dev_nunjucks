87853daf341241fecf3eb4cd10a815e366dad613
mamiline6 <ohmamirineoo6@gmail.com>
2021-02-28 14:09:58 +0900

    html（nunjucks）に変更を加えたら、 ブラウザが自動更新される

--- gulpfile.js
+++ gulpfile.js
@@ -2,14 +2,44 @@
 
 var gulp = require('gulp');
 var nunjucksRender = require('gulp-nunjucks-render');
+var browserSync = require('browser-sync').create();
 
-gulp.task('nunjucks', function () {
-  return gulp.src([
+var nunjucks = require('nunjucks');
+nunjucks.configure('./_resource/_layouts', {
+  noCache: true
+});
+
+gulp.task('nunjucks', (done) => {
+  gulp.src([
     '_resource/_layout/**/*.html',
     '!_resource/_layout/**/_*.html'
   ])
-    .pipe(nunjucksRender({
-      path: ['_resource/_layout/']
-    }))
-    .pipe(gulp.dest('./html'));
+  .pipe(nunjucksRender({path: '_resource/_layout/'}))
+  .pipe(gulp.dest('./html'))
+  done();
 });
+
+gulp.task('browser-sync', (done) => {
+  browserSync.init({
+    server: {
+      baseDir: './html/',
+      index: 'index.html',
+    },
+  });
+  done();
+});
+gulp.task('browser-reload', (done) => {
+  browserSync.reload();
+  done();
+});
+
+gulp.task('watch-files', (done) => {
+  gulp.watch('_resource/_layout/**/*.html', gulp.task('nunjucks'));
+  gulp.watch('./html/', gulp.task('browser-reload'));
+  done();
+});
+
+gulp.task('default', gulp.series(gulp.parallel('watch-files', 'browser-sync', 'nunjucks'), (done) => {
+  done();
+}));
+
