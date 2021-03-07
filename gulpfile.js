06d785d53bfd873c4b0ddd5ad13617d22aeb5a31
mamiline6 <ohmamirineoo6@gmail.com>
2021-03-07 23:39:20 +0900

    json 反映用の設定追加

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,8 @@
 
 var gulp = require('gulp');
 var nunjucksRender = require('gulp-nunjucks-render');
+var data = require('gulp-data');
+var fs = require('fs');
 var browserSync = require('browser-sync').create();
 
 gulp.task('nunjucks', (done) => {
@@ -11,6 +13,9 @@ gulp.task('nunjucks', (done) => {
     '!_resource/_layout/_*/**',
     '!_resource/_layout/**/_*.html'
   ])
+  .pipe(data(function(file) {
+    return JSON.parse(fs.readFileSync('./_resource/_layout/news.json'));
+  }))
   .pipe(nunjucksRender({path: '_resource/_layout'}))
   .pipe(gulp.dest('./html'))
   done();
@@ -32,6 +37,7 @@ gulp.task('browser-reload', (done) => {
 });
 
 gulp.task('watch-files', (done) => {
+  gulp.watch('_resource/**/*.json', gulp.task('nunjucks'));
   gulp.watch('_resource/**/*.html', gulp.task('nunjucks'));
   gulp.watch('./html/', gulp.task('browser-reload'));
   done();
